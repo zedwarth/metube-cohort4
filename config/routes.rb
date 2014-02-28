041fa222007d5e1c4ce52e2dd213c21805a54e65
Taylor Smith <twsmith89@gmail.com>
2014-02-28 16:21:09 -0600

    remove user views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Metube::Application.routes.draw do
   get "/videos", to: "videos#index"
   get "/videos/:id", to: "videos#show"
-
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
