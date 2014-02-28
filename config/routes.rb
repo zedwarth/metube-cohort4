0d37bf1051fdc5c4df871c969f00ee468f28a1e7
Taylor Smith <twsmith89@gmail.com>
2014-02-28 14:46:07 -0600

    add a root, routes to videos#new and videos#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Metube::Application.routes.draw do
+  root 'video#index'
   get "/videos", to: "videos#index"
+  get "/videos/new", to: "videos#new"
   get "/videos/:id", to: "videos#show"
+  post "/videos", to: "videos#create"
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
