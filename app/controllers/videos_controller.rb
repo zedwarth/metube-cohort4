573253b74861125bcea40b5ffbd4b34c22f3da06
Taylor Smith <twsmith89@gmail.com>
2014-02-28 16:16:22 -0600

    Add edit, update, and destroy actions

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,6 +17,22 @@ class VideosController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to root_path
+  end
+
   private
   def video_params
     params.require(:video).permit(:title, :description, :youtube_id)
