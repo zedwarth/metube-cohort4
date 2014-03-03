68fb6e6e8aaae6a0fcf682998839fabcd5cd301e
Taylor Smith <twsmith89@gmail.com>
2014-03-03 14:53:15 -0600

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Metube::Application.routes.draw do
+  root to: "videos#index"
   get "/videos", to: "videos#index"
   get "/videos/:id", to: "videos#show"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
